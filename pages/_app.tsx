af4eff87402d99e21ac6ab11b41b1b1422ed90cc
davidchanho <33166931+davidchanho@users.noreply.github.com>
2021-03-19 23:30:51 -0400

    add remove firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css";
 import { AppProvider } from "../context/context";
-import "../node_modules/slick-carousel/slick/slick-theme.css";
-import "../node_modules/slick-carousel/slick/slick.css";
 import GlobalStyle from "../utils/Global";
 
 function MyApp({ Component, pageProps }) {
