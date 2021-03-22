f1922ba0762553ee560ff7496c7760aab288a4ab
davidchanho <33166931+davidchanho@users.noreply.github.com>
2021-03-21 20:04:26 -0400

    add width, height to 404 img

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 function ErrorPage() {
   return (
-    <div>
-      <Image src="/svg/404.svg" alt="Error Page Not Found" />
-    </div>
+    <>
+      <Image src="/svg/404.svg" alt="Error Page Not Found" layout='fill' />
+    </>
   );
 }
 
