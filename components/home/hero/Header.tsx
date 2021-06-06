0bf1dad7039be88ad2135399e1364b785622a1ba
davidchanho <33166931+davidchanho@users.noreply.github.com>
2021-06-06 17:20:30 -0400

    add scaling title font size

--- components/home/hero/Header.tsx
+++ components/home/hero/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header className="ps-md-5">
-      <h1 className="title mb-3">
+      <h1 className="hero-title mb-3">
         Machines and business solutions that meet your needs.
       </h1>
       <a
