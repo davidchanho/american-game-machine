2aca8adc38826747671e5bb0a629078c55850a6a
davidchanho <33166931+davidchanho@users.noreply.github.com>
2021-06-07 17:03:58 -0400

    fix footer contact items fade in

--- components/shared/footer/ContactItem.tsx
+++ components/shared/footer/ContactItem.tsx
@@ -5,12 +5,10 @@ import AppearText from "../appear-text";
 function ContactItem({ Icon, contact, info, href, btnLabel }: IContact) {
   return (
     <div className="card h-100 mb-3">
-      <div className="card-header d-flex align-items-center mb-3">
-        <AppearText>
-          <Icon className="me-2" color="white" size={18} />
-          <h3 className="text-capitalize">{contact}</h3>
-        </AppearText>
-      </div>
+      <AppearText className="card-header d-flex align-items-center mb-3">
+        <Icon className="me-2" color="white" size={18} />
+        <h3 className="text-capitalize">{contact}</h3>
+      </AppearText>
       <div className="card-body mb-3">
         {info.map((i, index) => (
           <AppearText className="card-text" key={`${info}-${index}`}>
