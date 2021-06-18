91c4f5405ddaef273fc160f8c84783edef319a2c
davidchanho <33166931+davidchanho@users.noreply.github.com>
2021-06-18 19:55:53 -0400

    add google analytics

---
+++ config/googleAnalytics.ts
@@ -0,0 +1,13 @@
+import GA4React from "ga-4-react";
+
+const ga4react = new GA4React("G-PWFCPYM760", {}, [], 5000);
+
+ga4react.initialize().then(
+  (ga4) => {
+    ga4.pageview("path");
+    ga4.gtag("event", "pageview", "path");
+  },
+  (err) => {
+    console.error(err);
+  }
+);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import "bootstrap/dist/css/bootstrap-reboot.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import React from "react";
 import Layout from "../components/shared/layout";
+import "../config/googleAnalytics";
 import { AppProvider } from "../context/context";
 import "../styles/global.scss";
 
