e8836b1d995c5987fa2f367967713322b9a91c9c
davidchanho <33166931+davidchanho@users.noreply.github.com>
2021-06-22 18:32:10 -0400

    fixed product list item appear text

--- components/products/product-list/ProductListItem.tsx
+++ components/products/product-list/ProductListItem.tsx
@@ -10,22 +10,24 @@ function ProductListItem({ name, src }: IProduct) {
 
   return (
     <div className="col-5">
-      <AppearText
-        className="card shadow"
-        onMouseEnter={handleEnter}
-        onMouseLeave={handleLeave}
-      >
-        <Image className="card-img" src={src} alt={name} placeholder="blur" />
+      <AppearText>
+        <div
+          className="card shadow"
+          onMouseEnter={handleEnter}
+          onMouseLeave={handleLeave}
+        >
+          <Image className="card-img" src={src} alt={name} placeholder="blur" />
 
-        <p className="card-title">{name}</p>
+          <p className="card-title">{name}</p>
 
-        {enter && (
-          <div className="card-img-overlay d-flex align-items-center justify-content-center">
-            <button className="btn btn-light p-3 pointer">
-              <h5 className="card-title mb-0 fw-bold">View Details</h5>
-            </button>
-          </div>
-        )}
+          {enter && (
+            <div className="card-img-overlay d-flex align-items-center justify-content-center">
+              <button className="btn btn-light p-3 pointer">
+                <h5 className="card-title mb-0 fw-bold">View Details</h5>
+              </button>
+            </div>
+          )}
+        </div>
       </AppearText>
     </div>
   );
--- components/shared/footer/ContactInfo.tsx
+++ components/shared/footer/ContactInfo.tsx
@@ -7,7 +7,7 @@ function ContactInfo() {
     <div className="row row-cols-1 row-cols-md-3 my-4 mx-1">
       {contacts.map((contact) => (
         <div key={contact.id} className="col p-0">
-          <ContactItem {...contact}>{contact.Icon}</ContactItem>
+          <ContactItem {...contact} />
         </div>
       ))}
     </div>
