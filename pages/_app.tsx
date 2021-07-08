e34083bb3563a61bfa788f3b938f9c6bf2f40802
davidchanho <33166931+davidchanho@users.noreply.github.com>
2021-07-08 15:59:14 -0400

    remove extra layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,11 @@
 import "bootstrap/dist/css/bootstrap-reboot.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import Layout from "../components/layout";
 import "../config/googleAnalytics";
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
